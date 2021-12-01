63c1043640eed63af80e1b4ef1309463374e8d15
Marufjon94f <faith94nazarov@yandex.ru>
2021-12-01 17:46:06 +0500

    styles

--- src/components/ChannelPage/ChannelPage.jsx
+++ src/components/ChannelPage/ChannelPage.jsx
@@ -57,7 +57,7 @@ function ChannelPage() {
         </li>
 
         <li className="homel">
-          <Link className="links" to="./channel">
+          <Link className="links lineactive" to="./channel">
             Channels
           </Link>
         </li>
