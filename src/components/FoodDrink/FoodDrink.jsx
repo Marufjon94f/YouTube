e187f7de85ed58d4ee098b25cb88aff8f7592043
Marufjon94f <faith94nazarov@yandex.ru>
2021-12-01 06:42:26 +0500

    wwww

--- src/components/FoodDrink/FoodDrink.jsx
+++ src/components/FoodDrink/FoodDrink.jsx
@@ -2,26 +2,21 @@ import "./FoodDrink.scss";
 import Doughnout from "../../assets/img/doughnout.jpg";
 import { Link } from "react-router-dom";
 
-function FoodDrink({ title }) {
-  return (
-    <div>
-      <li className="food-item">
-        <Link className="links" to="/video">
-          <img
-            className="dough-img"
-            src={Doughnout}
-            alt="dougnout"
-            width="250"
-            height="150"
-          />
-          <h5 className="food-title">{title}</h5>
-          <div className="middle-text">
-            <p>45k views · 2 months ago</p>
-            <p>Food & Drink</p>
-          </div>
-        </Link>
-      </li>
-    </div>
-  );
+function FoodDrink ({title}) {
+    return(
+        
+<div>
+<li className="food-item">
+<Link className="links" to="/video">
+<img className="dough-img" src={Doughnout} alt="dougnout" width="250" height="150"/>
+<h5 className="food-title">{title}</h5>
+<div className="middle-text">
+<p>45k views  ·  2 months ago</p>
+<p>Food & Drink</p></div></Link>
+</li>
+</div>
+           
+        
+    )
 }
 export default FoodDrink;
